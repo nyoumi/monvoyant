a993c04bd4ab2def78418b63ed29054fdf462754
nyoumi <nyoumipaul@yahoo.fr>
2021-04-27 18:03:33 +0100

    facades

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
+        Illuminate\Html\HtmlServiceProvider::class, // The one I added
 
 
         /*
@@ -229,8 +229,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
+        'Form' => Illuminate\Html\FormFacade::class,
+
+        'Html' => Illuminate\Html\HtmlFacade::class, // I added
 
     ],
 
