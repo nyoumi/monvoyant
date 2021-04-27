aa03c42e322ac3e69027324ec67a88df0ea0d989
nyoumi <nyoumipaul@yahoo.fr>
2021-04-27 13:59:30 +0100

    nothing

--- resources/views/voyants/create.blade.php
+++ resources/views/voyants/create.blade.php
@@ -1,27 +1,48 @@
-@extends('layouts.master')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shark App</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
 
-@section('content')
+<nav class="navbar navbar-inverse">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="{{ URL::to('sharks') }}">shark Alert</a>
+    </div>
+    <ul class="nav navbar-nav">
+        <li><a href="{{ URL::to('sharks') }}">View All sharks</a></li>
+        <li><a href="{{ URL::to('sharks/create') }}">Create a shark</a>
+    </ul>
+</nav>
 
-<h1>Ajouter un voyant</h1>  
-<p class="lead">Remplissez les informations du voyant.</p>
-<hr>
+<h1>Create a shark</h1>
 
-{!! Form::open([
-    'route' => 'voyants.store'
-]) !!}
+<!-- if there are creation errors, they will show here -->
+{{ HTML::ul($errors->all()) }}
 
-<div class="form-group">
-    {!! Form::label('nom', 'Nom:', ['class' => 'control-label']) !!}
-    {!! Form::text('nom', null, ['class' => 'form-control']) !!}
+{{ Form::open(array('url' => 'sharks')) }}
+
+    <div class="form-group">
+        {{ Form::label('name', 'Name') }}
+        {{ Form::text('name', Input::old('name'), array('class' => 'form-control')) }}
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('email', 'Email') }}
+        {{ Form::email('email', Input::old('email'), array('class' => 'form-control')) }}
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('shark_level', 'shark Level') }}
+        {{ Form::select('shark_level', array('0' => 'Select a Level', '1' => 'Sees Sunlight', '2' => 'Foosball Fanatic', '3' => 'Basement Dweller'), Input::old('shark_level'), array('class' => 'form-control')) }}
+    </div>
+
+    {{ Form::submit('Create the shark!', array('class' => 'btn btn-primary')) }}
+
+{{ Form::close() }}
+
 </div>
-
-<div class="form-group">
-    {!! Form::label('biographie', 'Biographie:', ['class' => 'control-label']) !!}
-    {!! Form::textarea('biographie', null, ['class' => 'form-control']) !!}
-</div>
-
-{!! Form::submit('Sauvegarder', ['class' => 'btn btn-primary']) !!}
-
-{!! Form::close() !!
-
-@stop
+</body>
+</html>
