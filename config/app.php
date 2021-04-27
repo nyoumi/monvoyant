242e71b0b91180eea24d20899a1687ff04dd900d
nyoumi <nyoumipaul@yahoo.fr>
2021-04-27 13:09:25 +0100

    facade

--- config/app.php
+++ config/app.php
@@ -227,8 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form'=> Illuminate\Html\FormFacade\Form::class,
-        'Html'=> Illuminate\Html\HtmlFacade\Html::class  
+        
 
     ],
 
